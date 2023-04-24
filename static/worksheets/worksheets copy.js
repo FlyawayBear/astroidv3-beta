aa9bbbf604f2ed01c8446f82207fce3dabccff07
FlyawayBear <99303932+FlyawayBear@users.noreply.github.com>
2023-04-24 12:32:03 +1000

    v3 beta

---
+++ static/worksheets/worksheets copy.js
@@ -0,0 +1,30 @@
+function worksheets(value) {
+  try {
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        console.log("Service worker registration successful");
+
+        const url = value.trim();
+        if (!(url.startsWith("https://") || url.startsWith("http://"))) {
+          url = "https://" + url;
+        }
+        try {
+          sessionStorage.setItem("uvURL", __uv$config.encodeUrl(url));
+          console.log("Session storage successful");
+        } catch (error) {
+          console.error("Session storage failed:", error);
+        }
+        try {
+          location.href = "html-loader.html";
+          console.log("Redirection successful");
+        } catch (error) {
+          console.error("Redirection failed:", error);
+        }
+      });
+  } catch (error) {
+    console.error("Service worker registration failed:", error);
+  }
+}
