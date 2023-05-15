c7b05805d4a2630b72281c767e0f33e3de9d41a5
FlyawayBear <99303932+FlyawayBear@users.noreply.github.com>
2023-05-15 23:46:13 +1000

    removing old shi

--- static/sw-ol.js
+++
@@ -1,8 +0,0 @@
-importScripts("./uv/uv.bundle.js");
-importScripts("./worksheets/uv/uv.config.js");
-importScripts("./worksheets/uv/uv.sw.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get("userkey");
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
